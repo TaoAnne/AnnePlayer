78d98b92537117da14eb3c4e50b355aa26cc862a
Tao <ShyPhinehas@users.noreply.github.com>
2022-02-24 14:21:43 +0900

    Initial Commit

---
+++ AnnePlayer/AnnePlayer.h
@@ -0,0 +1,18 @@
+//
+//  AnnePlayer.h
+//  AnnePlayer
+//
+//  Created by Littlefox iOS Developer on 2022/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnnePlayer.
+FOUNDATION_EXPORT double AnnePlayerVersionNumber;
+
+//! Project version string for AnnePlayer.
+FOUNDATION_EXPORT const unsigned char AnnePlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnnePlayer/PublicHeader.h>
+
+
