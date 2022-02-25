b5347c03bae4bf0ca1c6bb6b9ffe0a5a933094e4
Tao <ShyPhinehas@users.noreply.github.com>
2022-02-25 14:55:05 +0900

    init

---
+++ AnnePlayerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnnePlayerExample
+//
+//  Created by Littlefox iOS Developer on 2022/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
